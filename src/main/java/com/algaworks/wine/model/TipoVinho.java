eacbae695ead77f1668a95dd1997bf912a3a3d02
Vinicius Silva <viniciusjns@live.com>
2016-08-19 09:11:09 -0300

    first commit

---
+++ src/main/java/com/algaworks/wine/model/TipoVinho.java
@@ -0,0 +1,19 @@
+package com.algaworks.wine.model;
+
+public enum TipoVinho {
+	
+	TINTO("Tinto"),
+	BRANCO("Branco"),
+	ROSE("Rosé");
+	
+	private String descricao;
+	
+	TipoVinho(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+}
---
+++ src/main/java/com/algaworks/wine/service/CadastroVinhoService.java
@@ -0,0 +1,33 @@
+package com.algaworks.wine.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.wine.model.Vinho;
+import com.algaworks.wine.repository.Vinhos;
+import com.algaworks.wine.storage.FotoStorage;
+
+@Service
+public class CadastroVinhoService {
+	
+	@Autowired
+	private Vinhos vinhos;
+	
+	@Autowired
+	private FotoStorage fotoStorage;
+	
+	public void salvar(Vinho vinho) {
+		this.vinhos.save(vinho);
+	}
+	
+	public String salvarFoto(Long codigo, MultipartFile foto) {
+		Vinho vinho = vinhos.findOne(codigo);
+		String nomeFoto = fotoStorage.salvar(foto);
+		vinho.setFoto(nomeFoto);
+		vinhos.save(vinho);
+		
+		return fotoStorage.getUrl(nomeFoto);
+	}
+
+}
